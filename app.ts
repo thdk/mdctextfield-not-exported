17c5a422d1f914e1f410c791b4ce4595f9950274
Thomas Dekiere <t.dekiere@gmail.com>
2018-12-04 18:59:08 +0100

    Reproduce MDCTextField not exported

---
+++ app.ts
@@ -0,0 +1,3 @@
+import { MDCTextField } from '@material/textfield';
+
+console.log(MDCTextField);
--- gulpfile.js
+++ gulpfile.js
@@ -2,79 +2,14 @@
 // Add dependencies
 const gulp = require('gulp');
 const rollup = require('rollup');
-const typescript = require('rollup-plugin-typescript');
-const sass = require('gulp-sass');
-const resolve = require('rollup-plugin-node-resolve');
-const commonJS = require('rollup-plugin-commonjs');
+const rollupstream = require('rollup-stream');
+const source = require('vinyl-source-stream');
 
-// Configuration
-const configuration = {
-    paths: {
-        src: {
-            html: './src/*.html',
-            css: [
-                './src/style/*.scss'
-            ],
-            js: 'src/app.ts'
-        },
-        dist: './dist'
-    }
-};
+gulp.task('bundle', function() {
+    return rollupstream('rollupstream.config.js')
+      .pipe(source("app.js")) // name of the output file
+      .pipe(gulp.dest('dist/js')); // location to put the output file
 
-// Gulp task to copy HTML files to output directory
-gulp.task('html', gulp.series(function (done) {
-    gulp.src(configuration.paths.src.html)
-        .pipe(gulp.dest(configuration.paths.dist));
-    done();
-}));
-
-// Gulp task to concatenate our scss files
-gulp.task('scss', gulp.series(function (done) {
-    gulp.src(configuration.paths.src.css)
-        .pipe(sass({
-            includePaths: ['node_modules/'] // added includePaths to resolve scss partials from node modules
-          }).on('error', sass.logError))
-        .pipe(gulp.dest(configuration.paths.dist + '/css'))
-    done();
-}));
-
-gulp.task('scss:watch', function () {
-    gulp.watch('./src/style/**/*.scss', gulp.series('scss'));
-})
-
-// based on example from https://rollupjs.org/guide/en (See Gulp section)
-gulp.task('tsc', () => {
-    return rollup.rollup({
-        input: configuration.paths.src.js,
-        // external: ['firebase', 'pubsub-js'],
-        plugins: [
-            typescript() // uses tsconfig.json, overwrite using: typescript({lib: ["es5", "es6", "dom"], target: "es5"})
-        ]
-    }).then(bundle => {
-        return bundle.write({
-            file: 'dist/js/app.js',
-            format: 'iife',
-            name: 'rollupBundle',
-            plugins: [
-                resolve(),
-                commonJS({
-                    include: 'node_modules/**'
-                })
-            ],
-            //   globals: {
-            //     'firebase': 'firebase',
-            //     'pubsub-js': 'PubSub'
-            //   }
-        });
-    });
 });
 
-gulp.task('tsc:watch', gulp.series(function (done) {
-    gulp.watch('./src/**/*.ts', gulp.series('tsc'));
-    done();
-}));
-
-// Gulp default task
-gulp.task('default', gulp.parallel(['html', 'tsc', 'scss']));
-
-gulp.task('watch', gulp.series("default", gulp.parallel(['tsc:watch', 'scss:watch'])));
+gulp.task('default', gulp.series('bundle'));
